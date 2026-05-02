8872d16d048dcba05f2a505e945047ed1ee0d1bd
denotaravys <denotaravys@gmail.com>
2026-05-02 14:57:53 +0200

    chore: restaurar template original e usar Java 21 para compatibilidade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,31 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    id("fabric-loom") version "1.7-SNAPSHOT"
-    id("org.jetbrains.kotlin.jvm") version "2.0.0"
+    id("net.fabricmc.fabric-loom-remap")
     `maven-publish`
+    id("org.jetbrains.kotlin.jvm") version "2.0.0"
 }
 
 version = providers.gradleProperty("mod_version").get()
 group = providers.gradleProperty("maven_group").get()
 
 repositories {
-    mavenCentral()
-    maven { url = uri("https://maven.fabricmc.net/") }
-    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
 }
 
 loom {
     splitEnvironmentSourceSets()
+
     mods {
         register("nexus-api-core") {
             sourceSet(sourceSets.main.get())
             sourceSet(sourceSets.getByName("client"))
         }
+    }
+}
+
+fabricApi {
+    configureDataGeneration {
+        client = true
     }
 }
 
@@ -29,18 +35,30 @@ dependencies {
     modImplementation("net.fabricmc:fabric-loader:${providers.gradleProperty("loader_version").get()}")
     modImplementation("net.fabricmc.fabric-api:fabric-api:${providers.gradleProperty("fabric_api_version").get()}")
     modImplementation("net.fabricmc:fabric-language-kotlin:${providers.gradleProperty("fabric_kotlin_version").get()}")
-    
-    // Essencial para o EngenhariaSonora (GLES30 / Pojav)
-    "modClientImplementation"("org.lwjgl:lwjgl-opengl:3.3.6-SNAPSHOT")
+}
+
+tasks.processResources {
+    inputs.property("version", version)
+    filesMatching("fabric.mod.json") {
+        expand("version" to version)
+    }
 }
 
 kotlin {
-    jvmToolchain(21)
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_21
+    }
+}
+
+java {
+    withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 publishing {
     publications {
-        create<MavenPublication>("mavenJava") {
+        register<MavenPublication>("mavenJava") {
             from(components["java"])
         }
     }
