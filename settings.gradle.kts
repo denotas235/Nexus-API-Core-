74b66b830519e492e9efe6d3e571ce9aee4a483c
denotaravys <denotaravys@gmail.com>
2026-05-02 05:55:39 +0200

    Alinhar ficheiros de build com o template oficial; mantidas adições essenciais

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,17 @@
 pluginManagement {
-	repositories {
-		maven {
-			name = "Fabric"
-			url = uri("https://maven.fabricmc.net/")
-		}
-		mavenCentral()
-		gradlePluginPortal()
-	}
-
-	plugins {
-		id("net.fabricmc.fabric-loom-remap") version providers.gradleProperty("loom_version")
-	}
+repositories {
+maven {
+name = "Fabric"
+url = uri("https://maven.fabricmc.net/")
+}
+mavenCentral()
+gradlePluginPortal()
 }
 
-// Should match your modid
+plugins {
+id("net.fabricmc.fabric-loom-remap") version providers.gradleProperty("loom_version")
+}
+}
+
 rootProject.name = "nexus-api-core"
 include(":nexus-modules:tdbr")
