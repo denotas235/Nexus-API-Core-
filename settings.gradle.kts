ab9ac73d5b70f9c5b8b17c58e666cc5670bcdd20
denotaravys <denotaravys@gmail.com>
2026-05-02 13:47:23 +0200

    fix: adicionar loom_version em gradle.properties e simplificar settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,13 @@
 pluginManagement {
-repositories {
-maven {
-name = "Fabric"
-url = uri("https://maven.fabricmc.net/")
-}
-mavenCentral()
-gradlePluginPortal()
-}
-
-plugins {
-id("net.fabricmc.fabric-loom-remap") version providers.gradleProperty("loom_version")
-}
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 
 rootProject.name = "nexus-api-core"
+include("nexus-tdbr")
