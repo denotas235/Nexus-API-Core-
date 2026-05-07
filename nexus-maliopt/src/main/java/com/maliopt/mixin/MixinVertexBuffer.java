1492a2a1f1a43f03836d51f62e817c77c268abac
denotas235 <recebeumoneyantonio@gmail.com>
2026-05-07 21:38:22 +0200

    fix: comentar MixinVertexBuffer (BuiltBuffer não existe em 1.21.1)

--- nexus-maliopt/src/main/java/com/maliopt/mixin/MixinVertexBuffer.java
+++ nexus-maliopt/src/main/java/com/maliopt/mixin/MixinVertexBuffer.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VertexBuffer.class)
-public class MixinVertexBuffer {
+// public class MixinVertexBuffer {
 
     @Inject(method = "upload", at = @At("HEAD"), cancellable = true)
     private void onUpload(BufferBuilder.BuiltBuffer builtBuffer, CallbackInfo ci) {
