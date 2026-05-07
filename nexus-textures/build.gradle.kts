c2de731ebe6d6343f5d98ac079c14321ffe0a7dc
denotas235 <recebeumoneyantonio@gmail.com>
2026-05-07 12:08:26 +0200

    fix: corrigir dependência do nexus-textures para módulo raiz

--- nexus-textures/build.gradle.kts
+++ nexus-textures/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     mappings(loom.officialMojangMappings())
     modImplementation("net.fabricmc:fabric-loader:0.19.2")
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.100.7+1.21.1")
-    implementation(project(":nexus-api-core"))
+    implementation(project(":"))
 }
 
 loom {
