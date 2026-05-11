43c5eac6a18f42a0b7eae39708cef7383f6f7133
denotas235 <recebeumoneyantonio@gmail.com>
2026-05-11 15:23:43 +0200

    feat: settings.gradle.kts incluindo nexus-streaming

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,9 @@
 pluginManagement {
     repositories {
-        maven {
-            name = "Fabric"
-            url = uri("https://maven.fabricmc.net/")
-        }
+        maven { name = "Fabric"; url = uri("https://maven.fabricmc.net/") }
         mavenCentral()
         gradlePluginPortal()
     }
-    plugins {
-        id("net.fabricmc.fabric-loom-remap") version providers.gradleProperty("loom_version")
-    }
 }
 rootProject.name = "nexus-api-core"
-include(":nexus-maliopt")
-include(":nexus-audio")
-include(":nexus-textures")
+include(":nexus-streaming")
