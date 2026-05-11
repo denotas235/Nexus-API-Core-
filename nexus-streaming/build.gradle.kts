e3f1f31ebab20874d37def11c71cbb3d121010c7
denotas235 <recebeumoneyantonio@gmail.com>
2026-05-11 15:23:33 +0200

    feat: estrutura inicial do modulo nexus-streaming

---
+++ nexus-streaming/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("fabric-loom") version "1.16-SNAPSHOT"
+    id("java")
+}
+version = property("mod_version").toString()
+group = property("maven_group").toString()
+base { archivesName = "nexus-streaming" }
+repositories {
+    maven("https://maven.fabricmc.net/")
+    mavenCentral()
+}
+dependencies {
+    minecraft("com.mojang:minecraft:${property("minecraft_version")}")
+    mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
+    modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_api_version")}")
+}
+loom {}
+tasks.processResources {
+    inputs.property("version", project.version)
+    filesMatching("fabric.mod.json") { expand(mapOf("version" to project.version)) }
+}
+tasks.withType<JavaCompile>().configureEach {
+    options.release = 21
+    options.encoding = "UTF-8"
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+    withSourcesJar()
+}
